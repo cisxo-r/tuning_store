e0e28e3d27bc0ec5c1a9710979fecbce64354ee0
cisxo-r <gpumpalev@mail.ru>
2023-01-25 16:48:38 +0300

    Доделана шапка, добавлены новые действующие кнопки в шапку,изменен шрифт

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home_page,name = 'home')
+    path('',home_page,name = 'home'),
+    path('about/',about,name = 'about'),
+    path('catalog/',items,name = 'items'),
+    path('favorite/',favorite,name = 'favorite'),
+    path('office/',personal_office,name = 'office')
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 
 def home_page(request):
     return render(request,'main/index.html')
+
+
+def about(request):
+    return render(request,'main/about.html')
+
+
+def items(request):
+    return render(request,'main/items.html')
+
+
+def favorite(request):
+    return render(request,'main/favorite.html')
+
+
+def personal_office(request):
+    return render(request,'main/office.html')
